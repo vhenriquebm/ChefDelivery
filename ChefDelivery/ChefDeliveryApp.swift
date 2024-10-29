87957442b3ebc813c82f037854e585586a9c5435
vhenriquebm <vhenriquebm@gmail.com>
2024-10-29 13:39:33 -0300

    Initial Commit

---
+++ ChefDelivery/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by Vitor Henrique Barreiro Marinho on 29/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChefDelivery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChefDelivery
+//
+//  Created by Vitor Henrique Barreiro Marinho on 29/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
