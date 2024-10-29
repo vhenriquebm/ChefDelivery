fda407612bd047b200f96a2a9e2cba8d767734b7
vhenriquebm <vhenriquebm@gmail.com>
2024-10-29 14:49:57 -0300

    HeaderView created

---
+++ ChefDelivery/HeaderView.swift
@@ -0,0 +1,47 @@
+//
+//  HeaderView.swift
+//  ChefDelivery
+//
+//  Created by Vitor Henrique Barreiro Marinho on 29/10/24.
+//
+
+import SwiftUI
+
+
+struct HeaderView: View {
+    
+    var body: some View {
+        
+        HStack {
+            
+            Spacer()
+            
+            
+            Button("R. Vergueiro, 3185") {
+                print ("Something")
+            }
+            .foregroundStyle(.black)
+            .fontWeight(.semibold)
+            .font(.subheadline)
+            
+            
+            Spacer()
+            
+            Button(action: {}) {
+                Image(systemName: "bell.badge")
+                    .foregroundStyle(Color.red)
+                    .padding(.horizontal)
+                    .font(.title3)
+            }
+        }
+    }
+}
+
+
+struct HeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeaderView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
